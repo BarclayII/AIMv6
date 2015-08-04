87b677565c50b883ba97a7c3bed15cff825bf9c5
davidgao <davidgao1001@gmail.com>
2015-08-05 02:51:08 +0800

    add readme to ARM interrupt handling

--- include/arch/armv7a-le/asm/irq.h
+++ include/arch/armv7a-le/asm/irq.h
@@ -11,6 +11,30 @@
 #ifndef _ASM_IRQ_H
 #define _ASM_IRQ_H
 
+/*
+ * PLEASE READ ME! I AM SERIOUS!
+ *
+ * There are 2 methods to interrupt an ARMv7A core
+ * from the outside world. IRQ and FIQ. Their control
+ * contains 3 parts. See Cortex-A9 MPCore TRM and ARM
+ * GIC Architecture specs before you go on.
+ *
+ * The core itself, through functions
+ * {irq,fiq}_{en,dis}able()
+ * They control CAN I BE INTERRUPTED BY ANY IRQ/FIQ
+ *
+ * The interrupt distributor, through functions
+ * TODO
+ * They control HOW TO DEAL WITH INTERRUPT REQUESTS
+ * An ARM processor must only have 1 distributor.
+ *
+ * The CPU Interface, through functions
+ * TODO
+ * They control WHAT INTERRUPT REQUEST DO I WANT TO
+ * SEE and performs actions like ACKNOWLEDGE and
+ * END_OF_INTERRUPT
+ */
+
 static inline void irq_enable()
 {
     asm volatile (
